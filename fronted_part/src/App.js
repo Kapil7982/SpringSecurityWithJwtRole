23877a5934ad7d6d49fed8cf4eef87e0ad42bf0a
Kapil7982 <rathor798299@gmail.com>
2023-06-23 15:06:28 +0530

    first part added

--- fronted_part/src/App.js
+++ fronted_part/src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [address, setAddress] = useState('');
+  const [role, setRole] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const customer = {
+      name,
+      email,
+      password,
+      address,
+      role,
+    };
+
+    axios
+      .post('http://localhost:8080/customers', customer)
+      .then((response) => {
+        console.log(response.data);
+        // Handle success
+        setName('');
+        setEmail('');
+        setPassword('');
+        setAddress('');
+        setRole('');
+        setErrorMessage('');
+      })
+      .catch((error) => {
+        console.error(error);
+        // Handle error
+        if (error.response && error.response.status === 409) {
+          setErrorMessage('An error accured, please try again later.');
+        } else {
+          alert('User already exists.');
+        }
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="navbar">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </div>
+      <div className="container">
+        <h1>Register Customer</h1>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <br />
+          <label>
+            Email:
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <br />
+          <label>
+            Address:
+            <input
+              type="text"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </label>
+          <br />
+          <label>
+            Role:
+            <input
+              type="text"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+            />
+          </label>
+          <br />
+          <button type="submit">Register</button>
+        </form>
+      </div>
     </div>
   );
 }
